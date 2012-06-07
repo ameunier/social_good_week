959f754d46a14bc0b1fa9f573797e5d33aba9f7c
Fabrice Carrega <fabrice.carrega@treize37.com>
2012-06-07 22:04:01 +0200

    Created application using Refinery CMS 2.0.4

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialGoodWeek::Application.config.secret_token = '22cdf915722843df2c3291c7ec7fc6eaed91c165007bd6222d233662ab56f4571eaae4fd54eb0f887d6208615a39c564053a446cbc102c01caf647350d1a5aaa'
